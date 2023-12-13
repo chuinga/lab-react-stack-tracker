32aa6f1db1546aee1c678a121ee93133b26e934e
Miguel Martins <mfproencamartins@gmail.com>
2023-12-13 17:26:03 +0000

    Starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
   return (
+    
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <div className="pages">        
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/company/:companyName' element={<CompanyPage />} />
+          <Route path='/tech/:technolySlug' element={<TechnologyPage />} />
+        </Routes>
+
+      </div>
     </div>
   );
 }
